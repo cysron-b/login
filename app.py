cf9e796ee96e1c034ef37120acd9e7205b076ff2
cysron-b <b.cysron@alustudent.com>
2024-07-26 21:01:17 +0200

    new

--- app.py
+++ app.py
@@ -239,5 +239,5 @@ def favicon():
 def get_products():
     return jsonify(products)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
